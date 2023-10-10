90cbca84dab66365f6e478d772fb5ad0c5ffc25e
Asad Siddiqui <38368758+asadsid95@users.noreply.github.com>
2023-10-09 23:04:45 -0400

    google and github oauth created; users not creating

--- app/auth/page.tsx
+++ app/auth/page.tsx
@@ -4,9 +4,16 @@ import Input from "@/components/Input"
 import axios from "axios"
 import { signIn } from "next-auth/react"
 import Image from "next/image"
+import { useRouter } from "next/navigation"
 import { useCallback, useState } from "react"
 
+import { FcGoogle } from 'react-icons/fc'
+import { FaGithub } from 'react-icons/fa'
+
+
 const Auth = () => {
+
+    const router = useRouter()
 
     const [email, setEmail] = useState('')
     const [name, setName] = useState('')
@@ -18,20 +25,6 @@ const Auth = () => {
         setVariant((currentVariant) => currentVariant === 'login' ? 'register' : 'login')
     }, [])
 
-    // makes api request for registering the user
-    const register = useCallback(async () => {
-        try {
-            // need to define this route
-            await axios.post('/api/register', {
-                email,
-                name,
-                password
-            })
-        } catch (err) {
-            console.log(err)
-        }
-    }, [email, name, password])
-
     // using signIn method from next-auth
     const login = useCallback(async () => {
         try {
@@ -41,10 +34,30 @@ const Auth = () => {
                 redirect: false,
                 callbackUrl: '/'
             })
+
+            router.push('/')
         } catch (err) {
             console.log(err)
         }
-    }, [email, password])
+    }, [email, password, router])
+
+    // makes api request for registering the user
+    const register = useCallback(async () => {
+        try {
+            // need to define this route
+            await axios.post('/api/register', {
+                email,
+                name,
+                password
+            })
+
+            login()
+        } catch (err) {
+            console.log(err)
+        }
+    }, [email, name, password, login])
+
+
 
     return (
         <div className="relative h-full w-full bg-[url('/images/hero.jpg')] bg-no-repeat bg-center bg-fixed bg-cover">
@@ -93,6 +106,20 @@ const Auth = () => {
                         >
                             {variant === 'register' ? 'Register' : 'Login'}
                         </button>
+                        <div className="flex flex-row items-center gap-4 mt-8 justify-center">
+                            <div
+                                onClick={() => signIn('google', { callbackUrl: '/' })}
+                                className="w-10 h-10 bg-white rounded-full flex items-center justify-center cursor-pointer hover:opacity-80 transition">
+                                <FcGoogle size={30} />
+
+                            </div>
+                            <div
+                                onClick={() => signIn('github', { callbackUrl: '/' })}
+                                className="w-10 h-10 bg-white rounded-full flex items-center justify-center cursor-pointer hover:opacity-80 transition">
+                                <FaGithub size={30} />
+
+                            </div>
+                        </div>
                         {variant === 'register' ?
                             <p
                                 className="text-neutral-500 mt-12"
